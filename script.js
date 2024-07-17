8c28727df613ae3f18f5fdc8c77a15ef8d94fe8f
THIAGO <thiagohenssuchi@gmail.com>
2024-07-17 16:44:05 -0400

    Site TBP finalizado sem resposividade

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#menu')
     header.classList.toggle('rolagem', window.scrollY > 90)
+    console.log(header);
 })
