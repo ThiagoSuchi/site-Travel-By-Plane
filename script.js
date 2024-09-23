551cfebdbba4764019c92ad7c2eb9c2fb26aff25
THIAGO <thiagohenssuchi@gmail.com>
2024-09-23 16:19:12 -0400

    mobile-TBP

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#menu')
     header.classList.toggle('rolagem', window.scrollY > 90)
-    console.log(header);
 })
